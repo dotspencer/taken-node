0619761446a9c2befcfd8ab6bd7c6712ed9d2d93
Spencer Smith <dontaskspencer@gmail.com>
2017-02-05 17:44:10 -0700

    Added keys.js

--- helpers/domains.js
+++ helpers/domains.js
@@ -1,4 +1,31 @@
 
 exports.lookup = (req, res) => {
-  res.json("this is api")
+  res.setHeader('Content-Type', 'application/json');
+
+  var word = req.query.word;
+  if(word == null){
+    res.send("{}");
+    return;
+  }
+
+  var options = {
+    method: 'POST',
+    url: 'https://api.godaddy.com/v1/domains/available',
+    qs: { checkType: 'FAST' },
+    headers:
+     { authorization: 'sso-key ' + keys.godaddy,
+       'content-type': 'application/json' },
+    body:
+     [ word + '.com',
+        word + '.org',
+        word + '.net',
+        word + '.io',
+        word + '.info' ],
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if (error) throw new Error(error);
+    res.send(body);
+  });
 }
