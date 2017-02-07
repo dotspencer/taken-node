e36a6b52221522435e06083dda4764ffeb2b88ab
Spencer Smith <dontaskspencer@gmail.com>
2017-02-06 19:41:21 -0700

    Updadated port

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/api', function(req, res) {
   domains.lookup(req, res);
 });
 
-var server = app.listen('3000', function() {
-  console.log("Server running on http://localhost:3000");
-});
+app.set('port', (process.env.PORT || 3000))
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}`)
+})
